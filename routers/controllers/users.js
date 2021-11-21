fea4554ef815b75b506568e910b80d9fd9cf91b1
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-21 12:01:07 +0300

    users controlle

--- routers/controllers/users.js
+++ routers/controllers/users.js
@@ -1,5 +1,41 @@
+const users = require("./../../db/models/users");
+
 const signUp = (req, res) => {
-  res.send("users route");
+  const user = new users(req.body);
+
+  users.find((err, data) => {
+    let key;
+    data.find((elm) => {
+      if (elm.userName === user.userName) {
+        key = 1;
+      }
+      if (elm.email === user.email) {
+        key = 2;
+      }
+    });
+
+    switch (key) {
+      case 1:
+        res.status(403).json("user name already exists");
+        break;
+
+      case 2:
+        res.status(403).json("email already exists");
+        break;
+
+      default:
+        user
+          .save()
+          .then((result) => {
+            res.status(200).json(user);
+          })
+          .catch((err) => {
+            res.json(err);
+          });
+
+        break;
+    }
+  });
 };
 
 module.exports = { signUp };
