76dfa3313057a4c848a8ce445bf3aaa927fa76c8
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-21 11:04:29 +0300

    creat project

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("hallo world");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server On Port ${PORT}`);
+});
